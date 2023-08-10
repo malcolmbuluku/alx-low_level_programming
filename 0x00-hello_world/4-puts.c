2c0aac70617b60322160c8970398e25489b441d7
malcolmbuluku <malcolmbuluku@gmail.com>
2023-08-10 15:55:58 -0700

    Printing in C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints a li ne with put functin
+ *
+ * returns: (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
