d312168a333048e2ffca89ecfe0701489b1b7125
malcolmbuluku <malcolmbuluku@gmail.com>
2023-08-17 10:07:41 -0700

    a function that multiplies two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,12 +3,14 @@
  * multip - the function checks for a digit 0 through
  * @a: a - is a variable
  * @b: b - is a variable
+ *
  * Return: Always 0.
  */
-int multip(int a, int b)
-{
-	int multip;
 
-	multip = a * b;
-	return (multip);
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
 }
