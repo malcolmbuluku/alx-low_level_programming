04c37a63571433eb11157add09ca594331be88f7
malcolmbuluku <malcolmbuluku@gmail.com>
2023-08-22 15:30:41 -0700

    a function that reverses a string.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - the function prints a string follwed by a new line
+ *
+ * @str: prints a string
+ */
+void _puts(char *str)
+{
+	int 1;
+
+	for (i = 0, str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
